8969c11ca66b45d80d95e7e338f75deb9ba5033c
Fabiano Matos <fgm@cin.ufpe.br>
2015-09-17 21:53:42 +0000

    criados metodos save, uptade destroy e get

--- src/app/class/modelbridge.rb
+++ src/app/class/modelbridge.rb
@@ -5,12 +5,36 @@ class ModelBridgeTemplate
     
     @model
 
+    def self.new(args = nil)
+        if args.nil?
+            @model.new
+        else
+            @model.new(args)
+        end
+    end
+    
+    def self.save
+        @model.save 
+    end
+    
+    def self.update(args = nil)
+        if args.nil?
+            @model.update
+        else
+            @model.update(args)
+        end
+    end
+
     def self.get(id)
-      @model.get(id) 
+        @model.find(id) 
     end
     
     def self.all
-      @model.all 
+        @model.all 
+    end
+    
+    def self.destroy
+        @model.destroy 
     end
     
     def self.where(conditions)
@@ -18,7 +42,7 @@ class ModelBridgeTemplate
     end
     
     def self.allLimetedOf(quant)
-      @model.all.limit(quant)
+        @model.all.limit(quant)
     end
     
     def self.count
